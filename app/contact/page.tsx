57dced37dcb51583be4fe1bc84aed334bcdbeb73
sidma64 <toprak.code@gmail.com>
2023-08-10 18:38:11 +0300

    My blog project starts

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <main className=" min-h-screen p-24">
+            <h2>My Contacts</h2>
+            <p>
+                <b>Phone Number:</b> +90 530 309 28 78
+            </p>
+        </main>
+    )
+}
